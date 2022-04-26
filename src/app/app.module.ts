5c98c88bbd096a27b6559eb9a0b152176953666c
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-04-26 12:23:41 -0300

    feat: add qrcode generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { QRCodeModule } from 'angularx-qrcode';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QrcodeGeneratorComponent } from './qrcode-generator/qrcode-generator.component';
+import { QrcodePicHandlerComponent } from './qrcode-generator/components/qrcode-pic-handler/qrcode-pic-handler.component';
+import { QrcodeSkeletonComponent } from './qrcode-generator/components/qrcode-pic-handler/components/qrcode-skeleton/qrcode-skeleton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrcodeGeneratorComponent,
+    QrcodePicHandlerComponent,
+    QrcodeSkeletonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    QRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
